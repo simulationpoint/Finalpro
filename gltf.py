ac4a3328f938f4d452bf14e3bd6717ba792d777d
simulationpoint <55375400+simulationpoint@users.noreply.github.com>
2020-12-08 02:33:12 -0500

    t

--- gltf.py
+++ gltf.py
@@ -17,7 +17,7 @@ MIN = 0
 # Inspired by WernerOrdonez2@github
 HOWMANYBYTES_V = VERTICES.nbytes
 HOWMANYBYTES_I = INDICES.nbytes
-with open("westworld.gltf", 'r') as f:
+with open("matrix.gltf", 'r') as f:
    lines = f.readlines()
 
    for i,l in enumerate(lines):
